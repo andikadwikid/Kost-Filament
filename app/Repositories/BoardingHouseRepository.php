150c888f37556c011bab9a6e55903b7a41f4f157
andika.dwiki <andika.dwiki@sucofindo.co.id>
2024-12-14 23:17:01 +0700

    Membuat Repository

---
+++ app/Repositories/BoardingHouseRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\BoardingHouseRepositoryInterface;
+use App\Models\BoardingHouse;
+use App\Models\City;
+
+class BoardingHouseRepository implements BoardingHouseRepositoryInterface
+{
+    public function getAllBoardingHouses($search = null, $city = null, $category = null)
+    {
+        $query = BoardingHouse::query();
+
+        if ($search) $query->where('name', 'like', '%' . $search . '%');
+
+        if ($city)
+            $query->whereHas(
+                'city',
+                fn($query) => $query->where('city', $city)
+            );
+
+        if ($category)
+            $query->whereHas(
+                'category',
+                fn($query) => $query->where('category', $category)
+            );
+
+        return $query->get();
+    }
+
+    public function getPopularBoardingHouses($limit = 3)
+    {
+        return BoardingHouse::withCount('transactions')->orderBy('transactions_count', 'desc')->take($limit)->get();
+    }
+
+    public function getBoardingHouseByCitySlug($slug)
+    {
+        return BoardingHouse::whereHas(
+            'city',
+            fn($query) => $query->where('slug', $slug)
+        )->get();
+    }
+}
